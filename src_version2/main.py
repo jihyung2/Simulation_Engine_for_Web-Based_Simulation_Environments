5e64afab4ef526c1bf6e87c66f7dffc6f05147d6
jihyung2 <wlgud1116@naver.com>
2023-07-29 04:44:55 +0900

    웹훅 연결 테스트 끝내면 이제 세부사항 만져야함

--- src_version2/main.py
+++ src_version2/main.py
@@ -1,116 +1,41 @@
 import importlib
-import time
-from fastapi import FastAPI, Request, Form, APIRouter
+
+from fastapi import FastAPI, Body, WebSocket, WebSocketDisconnect, websockets
+from typing import List
+from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse, HTMLResponse
-from pyevsim import BehaviorModelExecutor, Infinite, SystemSimulator
-import asyncio
-import requests
-import uvicorn
-import aiohttp
-
-# FastAPI와 Pyevsim 모두 정상적으로 불러오는지 확인
-try:
-    print("fastapi 생성 완료")
-    app = FastAPI()
-    router = APIRouter()
-    templates = Jinja2Templates(directory="templates")
-
-    @app.get("/", response_class=HTMLResponse)
-    async def index(request: Request):
-        return templates.TemplateResponse("index.html", {"request": request})
-except Exception as e:
-    app = None
-    print("FastAPI 에러 발생", str(e))
+from fastapi import FastAPI, Request, Form, APIRouter
 
 
-try:
-    pyevsim_code = importlib.import_module("pyevsim_code")
-    SimulationModel = pyevsim_code.SimulationModel
-    BehaviorModelExecutor = pyevsim_code.BehaviorModelExecutor
-    Infinite = pyevsim_code.Infinite
+pyevsim_code = importlib.import_module("pyevsim_code")
+MyRouter = pyevsim_code.MyRouter
+BehaviorModelExecutor = pyevsim_code.BehaviorModelExecutor
+Infinite = pyevsim_code.Infinite
+my_router = MyRouter(0, Infinite, "MyRouter", "Engine1")
 
-    simulation_model = SimulationModel(0, Infinite, "SimulationModel", "simulation")
-    print("pyevsim 생성 완료")
-    pyevsim_error = False
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-except ImportError:
-    simulation_model = None
-    pyevsim_error = True
-    print("pyevsim 모듈이 존재하지 않습니다.")
+class WebData(BaseModel):
+    checkboxes: List[str]
+    port: int
+    username: str
 
-except Exception as d:
-    simulation_model = None
-    pyevsim_error = True
-    print("pyevsim 에러 발생", str(d))
+@app.post("/start_simulation")
+async def start_simulation(data: WebData):
+    # 웹훅을 사용하여 시뮬레이션 처리를 시작합니다.
+    print(data)
+    my_router.simulate(data)
+    web_hook_receive(data)
+    if __name__ == "__main__":
+        import uvicorn
+        uvicorn.run(app, host="127.0.0.1", port=8700)
 
-# FastAPI와 Pyevsim 모두 정상적으로 불러온 경우
-if app and simulation_model:
-    @app.post("/start_simulation")
-    async def start_simulation(route_path: str = Form(...), data: str = Form(...)):
-        response = await simulation_model.simulate(route_path, data) # 동적 라우터 등록
-        print(response)
-        if isinstance(response, dict):
-            if response.get("method") == "POST":  # 동적 라우터 실행
-                new_route = simulation_model.POST_add_route_and_update_app(app, response["route_path"], response["data"])
-                print(new_route)
-
-
-            elif response.get("method") == "GET":
-                new_route = simulation_model.GET_add_route_and_update_app(app, response["route_path"], response["data"])
-                print(new_route)
-
-
-        elif isinstance(response, tuple):
-            url = str("http://localhost:8700"+response[0])
-            response = await fastapi_send(url, response[1], response[2])
-
-        return response
-
-
-    async def fastapi_send(url: str, data: dict, method: str):
-        async with aiohttp.ClientSession() as session:
-            if method == "GET":
-                async with session.get(url) as response:
-                    response_data = await response.json()
-            elif method == "POST":
-                async with session.post(url, json=data) as response:
-                    response_data = await response.json()
-
-        print(response_data)
-        return response_data
+@app.post("/{data.username}")
+async def web_hook_receive(data):
 
 
     if __name__ == "__main__":
-        uvicorn.run(app, host="127.0.0.1", port=8700)
-
-# Pyevsim만 정상적으로 불러온 경우
-elif simulation_model:
-    route_path = input("경로")
-    route_path = input("데이터")
-    async def start_simulation(route_path: str = Form(...), data: str = Form(...)):
-        response = await simulation_model.simulate(route_path, data)
-
-        if response["method"] == "POST":  # 동적 라우터 실행
-            new_route = simulation_model.POST_add_route_and_update_app(app, response["route_path"], response["data"])
-            print(new_route)
-
-        elif response["method"] == "GET":
-            new_route = simulation_model.GET_add_route_and_update_app(app, response["route_path"], response["data"])
-            print(new_route)
-
-        return response
-
-# FastAPI만 정상적으로 불러온 경우
-elif app:
-    @app.post("/start_simulation")
-    async def handle_web_hook(route_path: str = Form(...), data: str = Form(...)):
-        response = {"message": "pyevsim이 죽어서 fast api로만 보냅니다.", "data": data}
-        return response
-
-    print("Fast API만 실행됩니다.")
-    uvicorn.run(app, host="127.0.0.1", port=8700)
-
-# Pyevsim과 FastAPI 모두 에러가 발생한 경우
-else:
-    print("Pyevsim과 Fast API가 모두 에러가 발생했습니다.")
+        import uvicorn
+        uvicorn.run(app, host="127.0.0.1", port=data.port)
--- src_version2/pyevsim_code.py
+++ src_version2/pyevsim_code.py
@@ -1,123 +1,109 @@
-from tkinter.tix import Form
-from urllib.request import Request
+import time
+import datetime
 
-from pyevsim import BehaviorModelExecutor, Infinite, SystemSimulator
-from fastapi.responses import JSONResponse
-import asyncio
-from fastapi import FastAPI, APIRouter
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import JSONResponse, HTMLResponse
 import requests
+from fastapi import FastAPI, Request, Form, APIRouter
+from pyevsim import BehaviorModelExecutor, Infinite, SystemSimulator
+import pyevsim
 
-class SimulationModel(BehaviorModelExecutor):
+app = FastAPI()
+global a
+global realdata
+a = "dummy data"
 
+#이벤트 기반 시뮬레이션
+class MyRouter(BehaviorModelExecutor):
     def __init__(self, instance_time, destruct_time, name, engine_name):
-        super().__init__(instance_time, destruct_time, name, engine_name)
+        BehaviorModelExecutor.__init__(self, instance_time, destruct_time, name, engine_name)
         self.init_state("Wait")
         self.insert_state("Wait", Infinite)
-        self.insert_state("Simulate", 1)
-        self.routes = {}
-        self.app = FastAPI()  # FastAPI 애플리케이션 객체 생성
-        self.router = APIRouter
-    def route_show(self):
-        return self.app.routes
+        self.insert_state(a,1)
+        self.insert_state("simulation Start", 1)
 
-    def POST_add_route_and_update_app(self, app: FastAPI, route_path: str, response_data: dict):
-        @app.post(route_path)
-        async def dynamic_route():
-            return {"status": "Real POST success","method": "POST","route_path" : route_path, "data": response_data}
 
-        return app.routes[-1]
+    def ext_trans(self, port, msg):
+        if port == "start":
+            self._cur_state = "simulation Start"
+            # API 요청 처리 및 상태 변경
 
-    def GET_add_route_and_update_app(self, app: FastAPI, route_path: str, response_data: dict):
-        @app.get(route_path)
-        async def dynamic_route():
-            return {"status": "Real GET success", "method": "GET","route_path" : route_path, "data": response_data}
 
-        return app.routes[-1]
 
-    async def register_dynamic_routers(self, route_path, response_data, method):
-        router = APIRouter()
-        upload = "OK"
-        for route_obj in self.app.routes:
-            # 각 Route 객체로부터 속성들을 추출합니다.
-            path = route_obj.path
-            if path == route_path:
-                user_route_path = path
-                upload = None
+    def output(self):
+        now = datetime.datetime.now()
+        self.index = 0
+        self.item_list = []
+        print("==========================")
+        for i in range(len(self.menu)):
+            self._cur_state, self.temp = self.int_trans()
+            self.item_list.append(int(self.temp))
+            self.index += 1
 
-        if upload == None:
-            print("이미 등록된 경로입니다. 입력된 경로로 데이터를 보내 결과값을 반송합니다.")
+        print(self.menu)
+        print(self.item_list)
+        print(now)
+        self.web_hook_send(realdata)
+        self.put_db(self.item_list)
+        self.item_list = []
+        self.index = 0
 
-            return user_route_path, response_data, method
 
+        #if self._cur_state == "API":
+            # API 요청에 대한 처리 결과 반환
+            #return {"message": "This is the response for API request"}
+
+        #else:
+            # 잘못된 API 요청 처리
+            #return {"message": "Invalid API request"}
+    def web_hook_send(self, request_data):
+        webhook_url = f"http://127.0.0.1:{request_data.port}/{request_data.username}"
+        webhook_data = request_data.dict()
+        print(webhook_url)
+        response = requests.post(webhook_url, json = webhook_data)
+
+        if response.status_code == 200:
+            print("성공")
         else:
-            if method == "GET":
-                print("GET 라우터로 등록되었습니다.")
+            print("실패")
 
-                @router.get(route_path)
-                async def dynamic_get_route_handler(route_path, response_data):
-                    return {"route_path": route_path, "method": "GET", "data": response_data, "test": "GET success"}
+    def insert_list(self, arr):
+        setattr(self, "menu", arr)
+        setattr(self, "index", 0)
+        setattr(self, "temp", 0)
 
-                response_data = await dynamic_get_route_handler(route_path, response_data)
-                print(response_data)
+    def put_db(self, item_list):
+        pass
 
+    def int_trans(self):
+        now = datetime.datetime.now()
+        if self.menu[self.index] == str(a):
+            if str(a) == "/api":
+                return self.menu[self.index], 100
 
-            elif method == "POST":
-                print("POST 라우터로 등록되었습니다.")
+            else:
+                return self.menu[self.index], 0
 
-                @router.post(route_path)
-                async def dynamic_post_route_handler(route_path, response_data):
-                    return {"route_path": route_path, "method": "POST", "data": response_data, "test": "POST success"}
-                response_data = await dynamic_post_route_handler(route_path, response_data)
-                print(response_data)
+    def simulate(self, request_data):
+        global a
+        global realdata
+        realdata = request_data
+        a = str(request_data)
+        print("Simulating...")
+        menu = [a]
+        ss = SystemSimulator()
+        ss.register_engine("first", "REAL_TIME", 1)
+        ss.get_engine("first").insert_input_port("start")
+        gen = MyRouter(0, Infinite, "Gen", "first")
+        gen.insert_list(menu)
+        ss.get_engine("first").register_entity(gen)
+        ss.get_engine("first").coupling_relation(None, "start", gen, "start")
+        ss.get_engine("first").insert_external_event("start", None)
+        ss.get_engine("first").simulate()
 
-            self.app.include_router(router)
-            print("종료시 등록된 라우터:", self.app.routes)
-            print("정상적으로 route가 등록 및 실행되었습니다!")
+    def clear_data(self):
+        ss = SystemSimulator()
+        ss.register_engine("api", "DISCRETE_EVENT", 1)
 
-            return response_data
-
-    async def add_route(self, route_path, response_data):
-
-        parsed_data = self.parse_data(route_path, response_data)
-        self.routes = parsed_data
-        response = await self.register_dynamic_routers(self.routes["route_path"], self.routes["data"], self.routes["method"])
-
-        return response
-
-    def parse_data(self, route_path, response_data):
-        parsed_data = {"route_path": "", "data": {}, "method": ""}
-
-        if "?" in route_path:
-            parsed_data["method"] = "GET"
-            user_route_path, raw_data = route_path.split("?")
-            parsed_data["route_path"] = user_route_path
-
-            data_pairs = raw_data.split("&")
-            for data_pair in data_pairs:
-                name, value = data_pair.split("=")
-                parsed_data["data"][name] = value
-        else:
-            parsed_data["method"] = "POST"
-            parsed_data["route_path"] = route_path
-            parsed_data["data"] = {"data": response_data}
-
-        return parsed_data
-
-    async def simulate(self, route_path, response_data):
-        simulation_ss = SystemSimulator()
-        simulation_ss.register_engine("simulation", "REAL_TIME", 1)
-        simulation_ss.get_engine("simulation").insert_input_port("start_simulation")
-        simulation_model = SimulationModel(0, Infinite, "SimulationModel", "simulation")
-        simulation_ss.get_engine("simulation").register_entity(simulation_model)
-        simulation_ss.get_engine("simulation").coupling_relation(None, "start_simulation", simulation_model, "start_simulation")
-        print("시작시 등록된 라우터:", self.app.routes)
-        async def run_simulation():
-            simulation_ss.get_engine("simulation").simulate(5)
-            return await self.add_route(route_path, response_data)
-
-        result = await run_simulation()
-        return result
-
+    def destruct(self):
+        #미완성
+        self.clear_data()
 
