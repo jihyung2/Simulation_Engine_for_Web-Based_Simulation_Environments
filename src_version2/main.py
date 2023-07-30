f256c1a6caa907bb25828c27b5c0a7a6fc2eda3d
jihyung2 <wlgud1116@naver.com>
2023-07-30 17:59:20 +0900

    1차 완성 하지만 시뮬레이션에서 보낸 웹훅으로 바로 웹 페이지로 가는건 구현못함

--- src_version2/main.py
+++ src_version2/main.py
@@ -8,7 +8,6 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi import FastAPI, Request, Form, APIRouter
 
-
 pyevsim_code = importlib.import_module("pyevsim_code")
 MyRouter = pyevsim_code.MyRouter
 BehaviorModelExecutor = pyevsim_code.BehaviorModelExecutor
@@ -18,37 +17,56 @@ my_router = MyRouter(0, Infinite, "MyRouter", "Engine1")
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
 class WebData(BaseModel):
     checkboxes: List[str]
     port: int
     username: str
+
+
 class WebhookData(BaseModel):
     checkboxes: List[str]
     port: int
     username: str
-    time : str
+    time: str
+
+
+webhook_result = []
+
+
 @app.get("/", response_class=HTMLResponse)
 async def get_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.post("/start_simulation")
 async def start_simulation(data: WebData):
-    # 웹훅을 사용하여 시뮬레이션 처리를 시작합니다.
     print(data)
     await my_router.simulate(data)
-    print("finish!")
 
 
 @app.post("/{username}")
 async def receive_webhook(username: str, webhook_data: WebhookData):
+    global webhook_result
     print("웹훅 데이터 수신:")
     print(f"체크박스: {webhook_data.checkboxes}")
     print(f"포트: {webhook_data.port}")
     print(f"사용자 이름: {webhook_data.username}")
     print(webhook_data.time)
+    webhook_result.append(webhook_data.dict())
 
+
+@app.get("/get_result")
+async def get_result():
+    global webhook_result
+    if webhook_result:
+        data = webhook_result[-1]
+    else:
+        data = None
+    return JSONResponse(content={"result": data})
 
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8500)
